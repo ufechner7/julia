043a916d959fd5e6679ee23dc8d74a7b83f88e6f
Stefan Karpinski <stefan@karpinski.org>
2015-08-20 13:48:11 -0400

    new Char: up to 4 zero-padded UTF-8 bytes; basic change passes tests

--- base/char.jl
+++ base/char.jl
@@ -1,14 +1,31 @@
 # This file is a part of Julia. License is MIT: http://julialang.org/license
 
-convert(::Type{Char}, x::UInt32) = reinterpret(Char, x)
+function convert(::Type{UInt32}, c::Char)
+    u = reinterpret(UInt32, c)
+    u $= ifelse(
+        u <= 0x0000ffff,
+        ifelse(u <= 0x000000ff, 0x00000000, 0x0000c080),
+        ifelse(u <= 0x00ffffff, 0x00e08080, 0xf0808080),
+    )
+    ((u & 0x000000ff) >> 0) $ ((u & 0x0000ff00) >> 2) $
+    ((u & 0x00ff0000) >> 4) $ ((u & 0xff000000) >> 6)
+end
+
+function convert(::Type{Char}, u::UInt32)
+    u < 0x00200000 || throw(InexactError())
+    c = (u & 0x3f) | ((u << 2) & 0x3f00) | ((u << 4) & 0x3f0000) | ((u << 6) & 0x3f000000)
+    reinterpret(Char, ifelse(u <= 0x7f, u,
+        c | ifelse(u <= 0x000007ff, 0x0000c080,
+            ifelse(u <= 0x0000ffff, 0x00e08080, 0xf0808080))))
+end
+
 convert(::Type{Char}, x::Number) = Char(UInt32(x))
-convert(::Type{UInt32}, x::Char) = reinterpret(UInt32, x)
 convert{T<:Number}(::Type{T}, x::Char) = convert(T, UInt32(x))
 
 rem{T<:Number}(x::Char, ::Type{T}) = rem(UInt32(x), T)
 
-typemax(::Type{Char}) = Char(typemax(UInt32))
-typemin(::Type{Char}) = Char(typemin(UInt32))
+typemax(::Type{Char}) = reinterpret(Char, typemax(UInt32))
+typemin(::Type{Char}) = reinterpret(Char, typemin(UInt32))
 
 size(c::Char) = ()
 size(c::Char,d) = convert(Int, d) < 1 ? throw(BoundsError()) : 1
@@ -30,24 +47,44 @@ done(c::Char, state) = state
 isempty(c::Char) = false
 in(x::Char, y::Char) = x == y
 
-==(x::Char, y::Char) = UInt32(x) == UInt32(y)
+==(x::Char, y::Char) = eq_int(unbox(Char,x), unbox(Char,y))
 ==(x::Char, y::Integer) = UInt32(x) == y
 ==(x::Integer, y::Char) = x == UInt32(y)
 
-isless(x::Char, y::Char)    = isless(UInt32(x), UInt32(y))
+isless(x::Char, y::Char)    = ult_int(unbox(Char,x), unbox(Char,y))
 isless(x::Char, y::Integer) = isless(UInt32(x), y)
 isless(x::Integer, y::Char) = isless(x, UInt32(y))
 
 -(x::Char, y::Char) = Int(x) - Int(y)
--(x::Char, y::Integer) = reinterpret(Char, Int32(x) - Int32(y))
-+(x::Char, y::Integer) = reinterpret(Char, Int32(x) + Int32(y))
+-(x::Char, y::Integer) = Char(Int32(x) - Int32(y))
++(x::Char, y::Integer) = Char(Int32(x) + Int32(y))
 +(x::Integer, y::Char) = y + x
 
-bswap(x::Char) = Char(bswap(UInt32(x)))
-
 print(io::IO, c::Char) = (write(io, c); nothing)
+
+const ascii_esc = UInt8[0x61,0x62,0x74,0x6e,0x76,0x66,0x72]
+const hex_chars = UInt8['0':'9';'a':'z']
+
 function show(io::IO, c::Char)
-    print(io, '\'')
-    print_escaped(io, utf32(c), "'")
-    print(io, '\'')
+    u = reinterpret(UInt32, c)
+    if u == 0 || u == 27 || u == 92 || u == 39 || 7 <= u <= 13
+        b = u == 0  ? 0x30 :
+            u == 27 ? 0x65 :
+            u == 92 ? 0x5c :
+            u == 39 ? 0x27 :
+                      ascii_esc[u-6]
+        write(io, 0x27, 0x5c, b, 0x27)
+        return
+    elseif isprint(c)
+        write(io, 0x27, c, 0x27)
+    else
+        u = UInt32(c)
+        write(io, 0x27, 0x5c, u < 128 ? 0x78 : u < 65536 ? 0x75 : 0x55)
+        d = max(u < 128 ? 2 : 4, 8 - leading_zeros(u) >> 2)
+        while 0 < d
+            write(io, hex_chars[((u >> ((d -= 1) << 2)) & 0xf) + 1])
+        end
+        write(io, 0x27)
+    end
+    return
 end
